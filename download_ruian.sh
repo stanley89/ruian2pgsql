3d54389df949ae8a08eff7c386a66f1894fb250e
Stanislav Stipl <stanislav.stipl@gmail.com>
2021-03-05 12:15:15 +0100

    https

--- download_ruian.sh
+++ download_ruian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # http://vdp.cuzk.cz/
-DATE_UKSH='20210131'
-DATE_UVOH='20210203'
+DATE_UKSH='20210302'
+DATE_UVOH='20210302'
 TARGET='./zip'
 
 if [ -d $TARGET ]; then
@@ -13,9 +13,9 @@ fi
 
 for i in {500001..599999}
 do
-  wget -P $TARGET http://vdp.cuzk.cz/vymenny_format/soucasna/$DATE_UKSH\_OB_$i\_UKSH.xml.zip
+  wget -P $TARGET https://vdp.cuzk.cz/vymenny_format/soucasna/$DATE_UKSH\_OB_$i\_UKSH.xml.zip
 done
 
-wget -P $TARGET http://vdp.cuzk.cz/vymenny_format/specialni/$DATE_UVOH\_ST_UVOH.xml.zip
-wget -P $TARGET http://vdp.cuzk.cz/vymenny_format/soucasna/$DATE_UKSH\_ST_UKSH.xml.zip
+wget -P $TARGET https://vdp.cuzk.cz/vymenny_format/specialni/$DATE_UVOH\_ST_UVOH.xml.zip
+wget -P $TARGET https://vdp.cuzk.cz/vymenny_format/soucasna/$DATE_UKSH\_ST_UKSH.xml.zip
 
