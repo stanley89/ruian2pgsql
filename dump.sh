bf9b2c23a87f90562b1c68030ddf3db751c2ba7e
Stanislav Stipl <stanislav.stipl@gmail.com>
2021-03-05 04:03:12 +0100

    dump

--- dump.sh
+++ dump.sh
@@ -1,1 +1,6 @@
-docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_vusc -t rn_okres -t rn_obec -t rn_momc -t rn_ulice -t rn_vo ruian> ruian.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_vusc ruian>vusc.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_okres ruian>okres.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_obec ruian>obec.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_momc ruian>momc.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_ulice ruian>ulice.sql
+sudo docker-compose exec -u postgres db pg_dump --no-owner --no-acl -U ruian -t rn_vo ruian>vo.sql
